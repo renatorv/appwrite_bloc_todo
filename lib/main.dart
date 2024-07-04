adac9920825100e3fd8baabff3c56457a12cf257
renatorv <renatorv@gmail.com>
2024-07-03 22:15:53 -0300

    Finalizada aula GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:appwrite_bloc_todo/core/locators/locator.dart';
 import 'package:appwrite_bloc_todo/core/routes/routes.dart';
 import 'package:appwrite_bloc_todo/core/theme/app_theme.dart';
 import 'package:appwrite_bloc_todo/core/utils/app_strings.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  setupLocator();
+
   runApp(const MyApp());
 }
 
